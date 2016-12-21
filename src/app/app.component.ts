ff155f680735a1f6caff37cbc434fa42475709fb
Anna <anna@altsolution.net>
2016-12-21 11:02:58 +0200

    modified files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,91 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from './services/firebase.service';
+import { Contacts } from './Contacts';
+import { Types } from './Types';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [FirebaseService]
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+    contacts: Contacts[];
+    types: Types[];
+    appState: string;
+    activeKey: string;
+    isFavorite = false;
+    
+    activeName: string;
+    activePhone: string;
+    activeType: string;
+    activeIsFav: boolean;
+    
+    constructor(private firebaseService: FirebaseService) {
+    }
+    
+    ngOnInit(){
+        this.firebaseService.getContacts().subscribe(contacts => {
+            this.contacts = contacts;
+        });
+        
+        this.firebaseService.getCategories().subscribe(types => {
+            this.types = types;
+        });
+    }
+    
+    changeState(state, key){
+        if(key) {
+            this.activeKey = key;
+        }
+        this.appState = state;
+    }
+    
+    filterCategory(category){
+        this.firebaseService.getContacts(category).subscribe(contacts => {
+            this.contacts = contacts;
+        });
+    }
+    
+    setFav(){
+        this.isFavorite = !this.isFavorite;
+    }
+    
+    addBusiness(name:string, phone:string, type:string, isFavorite:boolean){
+        //var created_at = new Date().toString();
+        
+        var newContact = {
+            name,
+            phone,
+            type,
+            isFavorite
+        };
+        console.log(newContact);
+        this.firebaseService.addContact(newContact);
+        this.changeState('default');
+    }
+    
+    showEdit(contact){
+        this.changeState('edit', contact.$key);
+        this.activeName = contact.name;
+        this.activePhone = contact.phone;
+        this.activeType = contact.type;
+        this.activeIsFav = contact.isFavorite;
+    }
+    
+    updateContact(){
+        var updContact = {
+            name:this.activeName,
+            phone:this.activePhone,
+            type:this.activeType,
+            isFavorite:this.activeIsFav
+        }
+        this.firebaseService.updateContact(this.activeKey, updContact);
+        this.changeState('default');
+    }
+    
+    deleteContact(key){
+        this.firebaseService.deleteContact(key);
+        this.changeState('default');
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar.component';
+
+// Must export the config
+export const firebaseConfig = {
+  apiKey: 'AIzaSyChWc3al8Qs4q2GOD55hpJ8iPxiZbC8Log',
+  authDomain: 'addressbook-11bbd.firebaseapp.com',
+  databaseURL: 'https://addressbook-11bbd.firebaseio.com',
+  storageBucket: 'addressbook-11bbd.appspot.com'
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
     FormsModule,
     HttpModule
   ],
